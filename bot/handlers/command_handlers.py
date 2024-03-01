62ad100147b3f261ff4100fc4275c67d2470fa95
natalya tomova <natalyayurevna19@gmail.com>
2024-03-02 01:22:30 +1000

    Добавлен класс для группы состояний FSM

--- bot/handlers/command_handlers.py
+++ bot/handlers/command_handlers.py
@@ -1,9 +1,8 @@
 from datetime import timedelta
-from aiogram import Router, F, Bot
+from aiogram import Router, F
 from aiogram.filters.callback_data import CallbackData
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
-from aiogram.methods import SendMessage
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command, CommandStart, StateFilter
 from aiogram3_calendar import DialogCalendar
@@ -16,7 +15,9 @@ from bot.services import get_report
 command_router = Router()
 
 
-# создано хранилище данных
+# Cоздан класс для группы состояний FSM и экземпляры класса State,
+# обозначающие возможные состояния, в которых будет находиться
+# бот в разные моменты взаимодействия с пользователем."""
 class Query(StatesGroup):
     dt_from = State()
     dt_upto = State()
@@ -24,12 +25,14 @@ class Query(StatesGroup):
 
 
 @command_router.message(CommandStart())
-async def command_start(message: Message):
-    return await message.answer(text=LEXICON['/start'])
+async def command_start(message: Message, state: FSMContext) -> None:
+    """Этот хэндлер срабатывает на команду /start и очищает контекст"""
+    await state.clear()
+    await message.answer(text=LEXICON['/start'])
 
 
 @command_router.message(StateFilter(None), Command(commands=['date_from']))
-async def command_date(message: Message, state: FSMContext) -> Message:
+async def command_date_from(message: Message, state: FSMContext) -> Message:
     """Этот хэндлер срабатывает на команду /date_from,
     предлагает определить дату начала периода выборки.
     """
@@ -41,53 +44,57 @@ async def command_date(message: Message, state: FSMContext) -> Message:
 
 
 @command_router.message(Command(commands=['date_to']))
-async def command_date(message: Message, state: FSMContext) -> None:
+async def command_date_to(message: Message, state: FSMContext) -> Message:
     """Этот хэндлер срабатывает на команду /date_to,
     предлагает определить дату конца периода выборки.
     """
-    await message.answer(
+    await state.set_state(Query.dt_upto)
+    return await message.answer(
         text='Выберите дату окончания периода:',
         reply_markup=await DialogCalendar().start_calendar()
     )
-    await state.set_state(Query.dt_upto)
 
 
 @command_router.callback_query(Query.dt_from, DialogCalendarCallback.filter())
-async def process_dialog_calendar(callback_query: CallbackQuery,
-                                  callback_data: CallbackData,
-                                  state: FSMContext) -> None:
+async def process_dialog_calendar_from(callback_query: CallbackQuery,
+                                       callback_data: CallbackData,
+                                       state: FSMContext) -> Message:
     """Этот хэндлер обрабатывает CallbackQuery и показывает начальную дату.
     """
     selected, date = await DialogCalendar().process_selection(callback_query, callback_data)
     if selected:
         await state.update_data(dt_from=date)
-        await callback_query.message.answer(text=f'Вы выбрали дату начала периода: {date.strftime("%Y-%m-%d")}.')
+        return await callback_query.message.answer(text=f'Вы выбрали дату начала периода: '
+                                                        f'{date.strftime("%Y-%m-%d")}.')
 
 
 @command_router.callback_query(Query.dt_upto, DialogCalendarCallback.filter())
-async def process_dialog_calendar(callback_query: CallbackQuery, callback_data: CallbackData, state: FSMContext) -> None:
+async def process_dialog_calendar_to(callback_query: CallbackQuery,
+                                     callback_data: CallbackData,
+                                     state: FSMContext) -> None:
     """Этот хэндлер обрабатывает CallbackQuery и показывает конечную дату.
     """
     selected, date = await DialogCalendar().process_selection(callback_query, callback_data)
     if selected:
         await state.update_data(dt_upto=date + timedelta(hours=23, minutes=59))
-        await callback_query.message.answer(text=f'Вы выбрали дату окончания периода: {date.strftime("%Y-%m-%d")}.')
+        await callback_query.message.answer(text=f'Вы выбрали дату окончания периода: '
+                                                 f'{date.strftime("%Y-%m-%d")}.')
 
 
-@command_router.message(Command(commands=['type']))
-async def command_type(message: Message, state: FSMContext):
-    """Этот хэндлер срабатывает на команду /type,
+@command_router.message(Command(commands=['group_type']))
+async def command_type(message: Message, state: FSMContext) -> Message:
+    """Этот хэндлер срабатывает на команду /group_type,
     предлагает определить тип агрегирования.
     """
-    await message.answer(
+    await state.set_state(Query.group_type)
+    return await message.answer(
         text='Выберите тип группировки данных: ',
         reply_markup=inline_keyboard
     )
-    await state.set_state(Query.group_type)
 
 
 @command_router.callback_query(Query.group_type, F.data.in_({'hour', 'day', 'month'}))
-async def process_group_type(callback: CallbackQuery, state: FSMContext):
+async def process_group_type(callback: CallbackQuery, state: FSMContext) -> None:
     """Этот хэндлер обрабатывает CallbackQuery, отфильтрованный по
     значению Callbackdata, и записывает тип агрегирования в Query.
     """
@@ -96,22 +103,14 @@ async def process_group_type(callback: CallbackQuery, state: FSMContext):
 
 
 @command_router.message(Command(commands=['report']))
-async def command_report(message: Message, bot: Bot, state: FSMContext):
+async def command_report(message: Message, state: FSMContext) -> Message:
     """Этот хэндлер срабатывает на команду /report и возвращает
     агрегированные данные из БД в виде словаря.
     """
-    await message.answer(text='Отчёт:')
     data = await state.get_data()
     report = await get_report(data)
     if report['dataset']:
-        await bot.send_message(message.from_user.id, f'{report}')
+        return await message.answer(text=f'Отчёт:\n{report}')
     else:
-        await message.answer(text='За выбранный период нет данных.')
-    await state.clear()
+        return await message.answer(text='За выбранный период нет данных.')
 
-
-@command_router.message(Command(commands='help'))
-async def command_help(message: Message):
-    """Этот хэндлер срабатывает на команду /help.
-    """
-    await message.answer(text=LEXICON['/help'])
