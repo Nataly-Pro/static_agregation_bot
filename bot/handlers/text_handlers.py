64d95a7313202e0099b04a06dc7ed422fc4dd90b
natalya tomova <natalyayurevna19@gmail.com>
2024-03-02 01:29:19 +1000

    В process_some_text() добавлен блок try/except

--- bot/handlers/text_handlers.py
+++ bot/handlers/text_handlers.py
@@ -1,22 +1,20 @@
-from aiogram import Router, Bot, F
+from aiogram import Router, F
 from aiogram.types import Message
-from bot.config import load_config
 from bot.lexicon import LEXICON
 from bot.services import get_report, get_data_from_text
 
-config = load_config()
-bot = Bot(token=config.tg_bot.token)
+
 text_router = Router()
 formatted_router = Router()
 
 
 @formatted_router.message(F.text.startswith('{'))
-async def process_formatted_text(message: Message):
+async def process_formatted_text(message: Message) -> Message:
     """Этот хэндлер будет срабатывать на текстовые сообщения,
     имитирующие json формат.
     """
     data = message.text
-    return await bot.send_message(message.from_user.id, f'Отчёт:\n{await get_report(data)}')
+    return await message.answer(f'Отчёт:\n{await get_report(data)}')
 
 
 @text_router.message()
@@ -26,6 +24,6 @@ async def process_some_text(message: Message):
     """
     try:
         data = await get_data_from_text(message.text)
-        await bot.send_message(message.from_user.id, f'Отчёт:\n{await get_report(data)}')
+        return await message.answer(f'Отчёт:\n{await get_report(data)}')
     except (AttributeError, IndexError):
-        await message.answer(text=LEXICON['error'])
+        return await message.answer(text=LEXICON['error'])
