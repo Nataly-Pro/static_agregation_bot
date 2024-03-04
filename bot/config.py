a821b9893c0e0369d7e919e364bbb46a608efd9f
natalya tomova <natalyayurevna19@gmail.com>
2024-03-04 19:54:00 +1000

    Скорректирован BASE_DIR

--- bot/config.py
+++ bot/config.py
@@ -4,7 +4,8 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).parent.parent
+
 load_dotenv(BASE_DIR / '.env')
 
 
